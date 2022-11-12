e727a22c11ab6b492c73f9397b069d74ff6a345f
domssab <saballo.chantaldominique@gmail.com>
2022-11-13 01:15:35 +0800

    Input for option 1

--- Contact Tracing With Dictionary.py
+++ Contact Tracing With Dictionary.py
@@ -4,3 +4,15 @@ print("Contact Tracing"
       "\n1 -> Add an item"
       "\n2 -> Search"
       "\n3 -> Exit")
+
+#Main menu options
+mainmenu = 0
+while mainmenu in (0,1,2,3)
+      # Input for menu
+      mainmenu = int(input("Choose what to do: "))
+      if mainmenu == 1:
+            contact_info = []
+            full_name = input("Enter the full name: ")
+            age = int(input("Enter your age: "))
+            address = input("Enter your address: ")
+            phone_number = input("Enter the phone number: ")
