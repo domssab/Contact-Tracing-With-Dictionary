b1f1cb67afa5dfeb6bf0ea408add66cb28c26595
domssab <saballo.chantaldominique@gmail.com>
2022-11-13 11:39:48 +0800

    Adding elements to main menu

--- Contact Tracing With Dictionary.py
+++ Contact Tracing With Dictionary.py
@@ -1,14 +1,12 @@
 # Printing menu
-print("Contact Tracing"
-      "\nMenu: "
-      "\n1 -> Add an item"
-      "\n2 -> Search"
-      "\n3 -> Exit")
+print("===== CONTACT TRACING ====="
+      "\n|          Menu           |"
+      "\n|   1 -> Add an item      |"
+      "\n|   2 -> Search           |"
+      "\n|   3 -> Exit             |"
+      "\n===========================")
 
-contact_list = {
-      "Chantal Saballo": [20, "Padilla", 9954],
-      "Dominique Crespo": [19, "Lower Antipolo", 9653]
-}
+contact_list = {}
 
 #Main menu options
 mainmenu = 0
@@ -17,23 +15,23 @@ while mainmenu in (0,1,2,3):
       mainmenu = int(input("Choose what to do: "))
       # For Option 1
       if mainmenu == 1:
+            print("ADDING AN ITEM:")
             full_name = input("Enter the full name: ")
             age = int(input("Enter your age: "))
             contact_address = input("Enter your address: ")
             phone_number = input("Enter the phone number: ")
-            contact_info = ["Name:", full_name,
-                            "\nAge:", age,
-                            "\nAddress: ", contact_address,
-                            "\nPhone Number: ", phone_number]
+            contact_info = [full_name, age, contact_address, phone_number]
             print("Saved!")
             contact_list[full_name] = contact_info
       # For Option 2
       if mainmenu == 2:
+            print("SEARCHING FOR AN ITEM:")
             search_key = input("Enter the full name to search: ")
             print(contact_list[search_key])
 
       # For Option 3
       if mainmenu == 3:
+            print("THANK YOU!")
             break
 
 
