72e93c9e9a00d6af36b5d04431d380df11d12861
domssab <saballo.chantaldominique@gmail.com>
2022-11-13 11:23:14 +0800

    Editing of option 1 contents

--- Contact Tracing With Dictionary.py
+++ Contact Tracing With Dictionary.py
@@ -5,19 +5,10 @@ print("Contact Tracing"
       "\n2 -> Search"
       "\n3 -> Exit")
 
-contact_age = {
-      "Chantal Saballo": 20
-      "Dominique Crespo": 19
+contact_list = {
+      "Chantal Saballo": [20, "Padilla", 9954],
+      "Dominique Crespo": [19, "Lower Antipolo", 9653]
 }
-contact_address = {
-      "Chantal Saballo": Padilla
-      "Dominique Saballo": Lower Antipolo
-}
-contact_number = {
-      "Chantal Saballo": 0995
-      "Dominique Saballo": 0956
-}
-
 
 #Main menu options
 mainmenu = 0
@@ -30,10 +21,20 @@ while mainmenu in (0,1,2,3):
             age = int(input("Enter your age: "))
             contact_address = input("Enter your address: ")
             phone_number = input("Enter the phone number: ")
+            contact_info = ["Name:", full_name,
+                            "\nAge:", age,
+                            "\nAddress: ", contact_address,
+                            "\nPhone Number: ", phone_number]
             print("Saved!")
+            contact_list[full_name] = contact_info
       # For Option 2
       if mainmenu == 2:
+            search_key = input("Enter the full name to search: ")
+            print(contact_list[search_key])
 
+      # For Option 3
+      if mainmenu == 3:
+            break
 
 
 
